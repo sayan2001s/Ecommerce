771af0d88abf4ee41036d33806e22134a8abbfee
Sayan Sanfui <113054156+sayan2001s@users.noreply.github.com>
2024-05-17 00:16:36 +0530

    banner completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
+import Banner from "./components/ui/banner";
 import Button from "./components/ui/buttons";
 import Card from "./components/ui/cards";
 import demoImage from "./Image/Image.png";
+import demoImage2 from "./Image/image2.png";
+import ForwardArrow from './Image/Vector.png';
+import logo from './Image/image8.png';
+import watch from './Image/image 7.png';
 const App = () => {
   return (
     <div>
       <Card description="hi" name="DELL 21.5 inch Full HD Monitor (E2216HV)" price={1} orientation="horizontal" rating={4} image={demoImage} totalSales={6969} special="SALE"/>
+      <Banner title="Xiaomi True Wireless Earbuds" subtitle="Escape the noise, It’s time to hear the magic with Xiaomi Earbuds." image={watch} price={500} shopNowChildren={<><span>Shop Now</span><span><img src={ForwardArrow} alt="img_vector" /></span></>}
+      addTocartChildren={
+        <>
+        <span>Add to Cart</span>
+        <span><img src={ForwardArrow} alt="img_vector" /></span>
+        </>
+      }
+      viewDetailsChildren={
+        <>
+        <span>View Details</span>
+        <span><img src={ForwardArrow} alt="img_vector" /></span>
+        </>
+      }
+      logo={logo}
+      />
     </div>
   );
 };
